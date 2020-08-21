df23a128d6482ea95a8fa9447920c5d60c092f54
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-21 19:32:52 +0600

    props from object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF', price: '$10.99'}
+
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Person name='John' job='Software Developer'></Person>
+        <div>
+          <Product name={products[0].name} price={products[0].price}></Product>
+          <Product name={products[1].name} price={products[1].price}></Product>
+          <Product name={products[2].name} price={products[2].price}></Product>
+        </div>
+        {/* <Person name='John' job='Software Developer'></Person>
         <Person name='Gustavo' job='Businessman'></Person>
-        <Person name='Harry' job='Data Scientist'></Person>
+        <Person name='Harry' job='Data Scientist'></Person> */}
         
       </header>
     </div>
   );
+}
+
+function Product(props){
+
+  const productStyle = {
+    border : '1px solid gray',
+    borderRadius : '5px',
+    backgroundColor : 'lightgray',
+    height : '300px',
+    width : '250px',
+    float : 'left',
+    marginLeft : '10px'
+  }
+
+  return(
+    <div style={productStyle}>
+      <h2>{props.name} </h2>
+      <h1>{props.price}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
 }
 
 function Person(props){
