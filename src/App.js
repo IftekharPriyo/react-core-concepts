797b6e7dd515da9e99269e784d036042c4d67148
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-21 18:58:55 +0600

    props

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name='John' job='Software Developer'></Person>
+        <Person name='Gustavo' job='Businessman'></Person>
+        <Person name='Harry' job='Data Scientist'></Person>
         
       </header>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
   const personStyle = {
     border: '2px solid red',
     margin : '10px',
     padding : '30px',
-    borderRadius : '10px'
+    borderRadius : '10px',
+    width : '300px'
   }
   return (
     // <div style={{border:"2px solid red", margin: '10px'}}>
@@ -30,8 +31,8 @@ function Person(){
     //   <h3>Software Developer</h3>
     // </div>   
     <div style={personStyle}>
-      <h1>Name : John Doe</h1>
-      <h3>Software Developer</h3>
+      <h1>Name : {props.name}</h1>
+      <h3>Job : {props.job}</h3>
     </div> 
   )
 }
